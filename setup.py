ead21c5c77691cbff729c884125415d78645f3a4
Yuyuko Siagyouji <master.ind2008@gmail.com>
2022-08-05 06:21:23 +0300

    added setub.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='anime-folder-rename',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='GNU GPL 3.0',
+    author='yuyuko',
+    author_email='',
+    description=''
+)
